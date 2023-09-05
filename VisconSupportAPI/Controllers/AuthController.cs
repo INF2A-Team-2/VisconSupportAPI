ac9bcf417809ec8ee2d2062b720b1045aa0ffbb0
ZoutigeWolf <guuskamphuis@gmail.com>
2023-09-05 16:29:05 +0200

    Add login routes and encryption methods

---
+++ VisconSupportAPI/Controllers/AuthController.cs
@@ -0,0 +1,96 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using VisconSupportAPI.Data;
+using VisconSupportAPI.Models;
+
+namespace VisconSupportAPI.Controllers;
+
+[ApiController]
+[Route("api/login")]
+public class AuthController : ControllerBase
+{
+    private readonly ILogger<AuthController> _logger;
+
+    private readonly DatabaseContext _context;
+
+    private readonly IConfiguration _configuration;
+
+    public AuthController(ILogger<AuthController> logger, DatabaseContext context, IConfiguration configuration)
+    {
+        _logger = logger;
+        _context = context;
+        _configuration = configuration;
+    }
+
+    [HttpPost]
+    public ActionResult Login(UserCredentials credentials)
+    {
+        User? user = AuthenticateUser(credentials);
+
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        string token = GenerateJSONWebToken(user);
+        
+        return Ok(new { token = token });
+    }
+
+    [HttpGet]
+    [Authorize]
+    public ActionResult<string> Test()
+    {
+        return "Test";
+    }
+
+    private string GenerateJSONWebToken(User user)
+    {
+        SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+        SigningCredentials credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        Claim[] claims = new Claim[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Username)
+        };
+        
+        JwtSecurityToken token = new JwtSecurityToken(
+            _configuration["Jwt:Issuer"],
+            _configuration["Jwt:Issuer"], 
+            claims,
+            expires: DateTime.Now.AddHours(2), 
+            signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private User? AuthenticateUser(UserCredentials credentials)
+    {
+        User? user = _context.Users.SingleOrDefault(u => u.Username == credentials.Username);
+
+        if (user == null || user.PasswordHash != HashPassword(credentials.Password))
+        {
+            return null;
+        }
+
+        return user;
+    }
+    
+    private static string HashPassword(string input)
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes(input);
+        byte[] hash = SHA256.HashData(bytes);
+        return Convert.ToBase64String(hash);
+    }
+}
+
+public class UserCredentials
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
