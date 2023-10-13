c20da9645a8c9876e4716355f0a91e0ce7b35d59
Levi <vandaalenlevi@gmail.com>
2023-10-13 12:31:43 +0200

    Fixed typo in code from Marcel

--- VisconSupportAPI/Models/Issue.cs
+++ VisconSupportAPI/Models/Issue.cs
@@ -4,13 +4,13 @@ public class Issue
     public long Id {get; set;}
     public string Department {get; set;}
     public string Headline {get; set;}
-    public string Descprition{get; set;}
+    public string Description {get; set;}
 
     public Issue(long id, string dep, string head, string desc)
     {
         Id = id;
         Department = dep;
         Headline = head;
-        Descprition = desc;
+        Description = desc;
     }
 }
