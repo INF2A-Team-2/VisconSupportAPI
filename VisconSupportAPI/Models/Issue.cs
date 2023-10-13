31c0b01c8ee4f143adf9cf9e771ad80ea55de0e0
Levi <vandaalenlevi@gmail.com>
2023-10-13 12:25:41 +0200

    HeadLine => Headline

--- VisconSupportAPI/Models/Issue.cs
+++ VisconSupportAPI/Models/Issue.cs
@@ -3,14 +3,14 @@ public class Issue
 
     public long Id {get; set;}
     public string Department {get; set;}
-    public string HeadLine {get; set;}
+    public string Headline {get; set;}
     public string Descprition{get; set;}
 
     public Issue(long id, string dep, string head, string desc)
     {
         Id = id;
         Department = dep;
-        HeadLine = head;
+        Headline = head;
         Descprition = desc;
     }
 }
