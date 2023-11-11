1f8612f3ae73b00105771be14fafcdcc56c328d9
Luuk <luuk.volkering@gmail.com>
2023-11-11 18:54:21 +0100

    Added a route to read an uploaded file with machines. The template for the csv will be in the frontend repo.

---
+++ VisconSupportAPI/Controllers/ImportFileController.cs
@@ -0,0 +1,80 @@
+using System.Globalization;
+using CsvHelper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VisconSupportAPI.Data;
+using VisconSupportAPI.Models;
+
+namespace VisconSupportAPI.Controllers;
+
+[ApiController]
+[Route("api/import")]
+public class ImportFileController: BaseController
+{
+    public ImportFileController(ILogger<AuthController> logger, DatabaseContext context, IConfiguration configuration) : base(logger, context, configuration)
+    {
+    }
+
+    [HttpPost]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult ImportMachines(IFormFile formFile)
+    {
+        User? user = GetUserFromClaims();
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        if (user.Type != AccountType.Admin)
+        {
+            return Forbid();
+        }
+
+        if (formFile.Length <= 0 || !formFile.FileName.EndsWith(".csv")) return BadRequest();
+
+        using (var reader = new StreamReader(formFile.OpenReadStream())) 
+        {
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<ReadMachine>();
+            var readMachines = records.ToList();
+            if (!readMachines.Any()) return NoContent();
+
+            foreach (var machine in readMachines)
+            {
+                var currUser = Context.Users.First(h => h.Id == machine.UserID);
+                var mach = Context.Machines.Add(new Machine() { Name = machine.Name }).Entity;
+                Context.Entry(currUser).Collection(h => h.Machines).Load();
+                currUser.Machines.Add(mach);
+            }
+        }
+
+        return Ok();
+    }
+
+    // [HttpGet]
+    // [Authorize]
+    // public ActionResult GetExample()
+    // {
+    //     var records = Context.Users.First(h => h.Id == 1);
+    //     Context.Entry(records).Collection(h => h.Machines).Load();
+    //     using (var writer = new StreamWriter("file.csv"))
+    //     using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+    //     {
+    //         csv.WriteRecords(records.Machines);
+    //     }
+    //
+    //     return Ok();
+    // }
+}
+
+public class ReadMachine
+{
+    public long UserID { get; set; }
+    public string Name { get; set; }
+}
