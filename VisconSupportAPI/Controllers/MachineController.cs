282bccb9178674d9e9944b9e16b9d0b6525a4f6f
ZoutigeWolf <guuskamphuis@gmail.com>
2023-11-14 09:24:48 +0100

    move csv imort to MachineController.cs

--- VisconSupportAPI/Controllers/MachineController.cs
+++ VisconSupportAPI/Controllers/MachineController.cs
@@ -1,10 +1,10 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
-using Microsoft.EntityFrameworkCore;
 using VisconSupportAPI.Controllers;
 using VisconSupportAPI.Data;
 using VisconSupportAPI.Models;
+using CsvHelper;
 
 [ApiController]
 [Route("api/machines")]
@@ -36,4 +36,53 @@ public class MachineController : BaseController
         
         return Ok(Context.Machines);
     }
+    
+    [HttpPost("import")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult ImportMachines([FromForm]IFormFile formFile)
+    {
+        User? user = GetUserFromClaims();
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        if (user.Type != AccountType.Admin)
+        {
+            return Forbid();
+        }
+
+        if (formFile.Length <= 0 || !formFile.FileName.EndsWith(".csv")) return BadRequest();
+
+        using (var reader = new StreamReader(formFile.OpenReadStream())) 
+        {
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var readMachines = csv.GetRecords<ReadMachine>().ToList();
+            if (!readMachines.Any()) return NoContent();
+
+            foreach (var machine in readMachines)
+            {
+                var currUser = Context.Users.FirstOrDefault(h => h.Id == machine.UserId);
+                if(currUser == null) continue;
+                Context.Entry(currUser).Collection(h => h.Machines).Load();
+                var exists = Context.Machines.FirstOrDefault(h => h.Name == machine.Name);
+                currUser.Machines.Add(exists ?? new Machine() { Name = machine.Name });
+            }
+
+            Context.SaveChanges();
+        }
+
+        return Ok();
+    }
+}
+
+public class ReadMachine
+{
+    public long UserId { get; set; }
+    public string Name { get; set; }
 }
