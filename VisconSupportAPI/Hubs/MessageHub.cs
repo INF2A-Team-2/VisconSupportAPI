d9bf2a75535d93ccd7344cd97eef26e511686158
Luuk <luuk.volkering@gmail.com>
2023-11-20 19:49:43 +0100

    simplify messages

--- VisconSupportAPI/Hubs/MessageHub.cs
+++ VisconSupportAPI/Hubs/MessageHub.cs
@@ -1,8 +1,5 @@
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using VisconSupportAPI.Data;
-using VisconSupportAPI.Models;
 
 namespace VisconSupportAPI.Hubs;
 
@@ -11,23 +8,11 @@ public class MessageHub : Hub
     static readonly DatabaseContext DataContext;
     public async Task SendMessage(string issueId, string message)
     {
-        string? userId = Context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
-        if (userId == null)
-        {
-            return;
-        }
         await Clients.Group(issueId).SendAsync("message", message);
     }
     public async Task AddToGroup(string groupName)
     {
-        string? userId = Context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
-        if (userId == null) return;
-        User? user = DataContext.Users.FirstOrDefault(h => h.Username == userId);
-        if (user == null) return;
-        if(user.Type is AccountType.Admin or AccountType.Helpdesk || DataContext.Issues.FirstOrDefault(h => h.UserId == user.Id) != null)
-            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
         await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
     }
