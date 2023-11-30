877001d5e88e31c315a4b8a71d91e91392781186
ZoutigeWolf <guuskamphuis@gmail.com>
2023-11-30 14:15:16 +0100

    auth tests

---
+++ VisconSupportAPI.Tests/ServiceTests/AuthTests.cs
@@ -0,0 +1,61 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using VisconSupportAPI.Data;
+using VisconSupportAPI.Handlers;
+using VisconSupportAPI.Services;
+using VisconSupportAPI.Types;
+
+namespace VisconSupportAPI.Tests;
+
+[Collection("AuthTests")]
+public class AuthTests : ServiceTest
+{
+    public AuthTests() : base()
+    {
+        
+    }
+
+    [Fact]
+    public void TestJsonWebToken()
+    {
+        User? user = Services.Users.GetById(1);
+        
+        Assert.NotNull(user);
+        
+        string jwt = Services.Auth.GenerateJsonWebToken(user);
+
+        JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+        JwtSecurityToken token = handler.ReadJwtToken(jwt);
+        
+        string? name = token.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Sub)?.Value;
+        
+        Assert.NotNull(name);
+        Assert.Equal(user.Username, name);
+    }
+
+    [Theory]
+    [InlineData("customer", "    ", true)]
+    [InlineData("employee", "    ", true)]
+    [InlineData("admin", "apple", false)]
+    public void TestAuthenticateUser(string username, string password, bool valid)
+    {
+        User? user = Services.Auth.AuthenticateUser(new UserCredentials()
+        {
+            Username = username,
+            Password = password
+        });
+        
+        Assert.Equal(valid, user != null);
+    }
+
+    [Theory]
+    [InlineData("    ", "Gg9WTdxgOUV7L7JrPWoxbBXrogqIZEmEfDIQw1ghppM=")]
+    public void TestHashPassword(string password, string result)
+    {
+        string hashedPassword = AuthService.HashPassword(password);
+        
+        Assert.Equal(result, hashedPassword);
+    }
+}
--- VisconSupportAPI/Controllers/Controller.cs
+++ VisconSupportAPI/Controllers/Controller.cs
@@ -38,5 +38,4 @@ public abstract class Controller<T, THandler> : ControllerBase
         
         return user;
     }
-
 }
