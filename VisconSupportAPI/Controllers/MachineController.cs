cab67cbad9685bbb7b646286d3c59ea8aed526ba
ZoutigeWolf <guuskamphuis@gmail.com>
2023-11-30 16:45:11 +0100

    get specific machine

--- VisconSupportAPI/Controllers/MachineController.cs
+++ VisconSupportAPI/Controllers/MachineController.cs
@@ -23,6 +23,14 @@ public class MachineController : Controller<MachineController, MachineHandler>
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public ActionResult<List<Machine>> GetMachines() => Handler.GetAllMachines(GetUserFromClaims());
+    
+    [HttpGet("{machineId:int}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<Machine> GetMachine(int machineId) => Handler.GetMachine(GetUserFromClaims(), machineId);
 
     [HttpPost("import")]
     [Authorize]
