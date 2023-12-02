451b68f2fa1ec6621fa91bb0ed055a571cbe3686
marcelboo <101633688+marcelboo@users.noreply.github.com>
2023-12-02 14:27:39 +0100

    start for logging, got stuck on the service part

---
+++ VisconSupportAPI/Models/Log.cs
@@ -0,0 +1,22 @@
+﻿namespace VisconSupportAPI.Models;
+
+public class Log
+{
+    public int Id { get; set; }
+    
+    public DateTime TimeStamp { get; set; }
+
+    public int AuthorId { get; set; }
+    
+    public string Description { get; set; }
+    
+    public int? IssueId { get; set; }
+    
+    public int? UserId { get; set; }
+    
+    public int? MachineId { get; set; }
+    
+    public int? MessageId { get; set; }
+    
+    public int? AttachmentId { get; set; }
+}
---
+++ VisconSupportAPI/Services/LogService.cs
@@ -0,0 +1,23 @@
+﻿using VisconSupportAPI.Data;
+using VisconSupportAPI.Controllers;
+using VisconSupportAPI.Models;
+
+namespace VisconSupportAPI.Services;
+
+public class LogService : Service
+{
+    public LogService(DatabaseContext context, IConfiguration configuration, ServicesList services) : base(context,
+        configuration, services)
+    {
+        
+    }
+    
+    public Log? GetById(int id) => Context.Logs.FirstOrDefault(i => i.Id == id);
+    
+    public List<Log> GetAll() => Context.Logs.ToList();
+
+    public Log LoggingMachine(Machine machine)
+    {
+        
+    }
+}
