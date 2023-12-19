2dc180a7ad9f9107cc55bc0cea3fd2ffdf3781c8
ZoutigeWolf <guuskamphuis@gmail.com>
2023-12-19 11:08:13 +0100

    balls

--- VisconSupportAPI/Controllers/ReportController.cs
+++ VisconSupportAPI/Controllers/ReportController.cs
@@ -8,7 +8,7 @@ using VisconSupportAPI.Types;
 namespace VisconSupportAPI.Controllers;
 
 [ApiController]
-[Route("api/units")]
+[Route("api/reports")]
 public class ReportController : Controller<ReportController, ReportHandler>
 {
     public ReportController(ILogger<ReportController> logger, DatabaseContext context, IConfiguration configuration)
@@ -23,7 +23,7 @@ public class ReportController : Controller<ReportController, ReportHandler>
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
     public ActionResult<List<Report>> GetReports() => Handler.GetAllReports();
 
-    [HttpGet("{unitId:int}")]
+    [HttpGet("{reportId:int}")]
     [Authorize]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
