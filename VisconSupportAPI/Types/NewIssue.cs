fc72102584c225418fb0b03c81c2d89b3cf4c036
Luuk <luuk.volkering@gmail.com>
2024-01-10 13:42:47 +0100

    getmachine ontverkankert

--- VisconSupportAPI/Types/NewIssue.cs
+++ VisconSupportAPI/Types/NewIssue.cs
@@ -9,7 +9,6 @@ public class NewIssue
     public string Actual { get; set; }
     public string Expected { get; set; }
     public string Tried { get; set; }
-    public string PhoneNumber { get; set; }
     public string Headline { get; set; }
     public int MachineId { get; set; }
 }
