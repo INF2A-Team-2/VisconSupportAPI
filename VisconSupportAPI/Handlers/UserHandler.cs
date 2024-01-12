59a2ff8d732981f4a10b6e518cca59ea4804c75d
Luuk <luuk.volkering@gmail.com>
2024-01-12 15:07:50 +0100

    je kan jezelf nu niet meer zien in de users lijst

--- VisconSupportAPI/Handlers/UserHandler.cs
+++ VisconSupportAPI/Handlers/UserHandler.cs
@@ -21,9 +21,9 @@ public class UserHandler : Handler
 
         if (user.Type == AccountType.Helpdesk)
         {
-            return new OkObjectResult(Context.Users.Where(u => u.UnitId == user.UnitId).ToList());
+            return new OkObjectResult(Context.Users.Where(u => u.UnitId == user.UnitId && u.Id != user.Id).ToList());
         }
-        return new OkObjectResult(Services.Users.GetAll());
+        return new OkObjectResult(Services.Users.GetAll().Where(u => u.Id != user.Id));
     }
     
     public ActionResult<User> GetUserById(User? user, int userId)
