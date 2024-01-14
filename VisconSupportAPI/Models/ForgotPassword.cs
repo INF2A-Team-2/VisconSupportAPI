8f9bfd7d2b9ed66f44a355be67f7cf34382dc7d5
Luuk <luuk.volkering@gmail.com>
2024-01-14 20:17:43 +0100

    dom foutje verholpen

--- VisconSupportAPI/Models/ForgotPassword.cs
+++ VisconSupportAPI/Models/ForgotPassword.cs
@@ -2,8 +2,8 @@ namespace VisconSupportAPI.Models;
 
 public class ForgotPassword
 {
+    public int Id { get; set; }
     public int UserId { get; set; }
     public string Token { get; set; }
-    
     public User User { get; set; }
 }
